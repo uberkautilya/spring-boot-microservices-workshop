5177eb034a05116c5115b36358dfbd6a3a8e6413
Mithun <mithuncm@gmail.com>
2023-01-23 19:01:55 +0530

    BullkHead design pattern support from Hystrix with threadPoolKey and threadPoolProperties values in the @HystrixCommand annotation

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/hystrixproxy/MovieInfoProxy.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/hystrixproxy/MovieInfoProxy.java
@@ -13,11 +13,19 @@ public class MovieInfoProxy {
     @Autowired
     private RestTemplate restTemplate;
 
+    /**
+     * threadPoolKey and threadPoolProperties allow for the Bulkhead pattern solution
+     * Each service will get a fixed number of available threads, which are exhaustible independently
+     * Service may also share a thread pool if so desired
+     */
     @HystrixCommand(fallbackMethod = "getMovieInformationFallback", commandProperties = {
             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
             @HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value = "5"),
             @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
             @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+    }, threadPoolKey = "movieInfoPool", threadPoolProperties = {
+            @HystrixProperty(name = "coreSize", value = "20"),
+            @HystrixProperty(name = "maxQueueSize", value = "10")
     })
     public Movie getMovieInformation(Rating rating) {
         return restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
