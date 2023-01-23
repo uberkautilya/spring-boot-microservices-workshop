3d7cf734eba0378b2e106079e25a5fa80a7018cf
Mithun <mithuncm@gmail.com>
2023-01-23 19:18:53 +0530

    @EnableHystrixDashboard annotation to provide the hystrix dashboard. Also provided the property management.endpoint.web.exposure.include=hystrix.stream so this data is available to the hystrix dashboard. Hence, the dependencies hystrix-dashboard and actuator are added

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,13 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * @EnableHystrixDashboard makes this application also provide a UI of hystrix results
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
